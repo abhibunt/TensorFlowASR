fix: numerical stability

tensorflow_asr/losses/ctc_loss.py
@@ -24,36 +24,13 @@ class CtcLoss(tf.keras.losses.Loss):
         super(CtcLoss, self).__init__(reduction=tf.keras.losses.Reduction.NONE, name=name)
         self.blank = blank
 
-    def call(
-        self,
-        y_true,
-        y_pred,
-    ):
-        return ctc_loss(
-            y_pred=y_pred["logits"],
-            input_length=y_pred["logits_length"],
-            y_true=y_true["labels"],
+    def call(self, y_true, y_pred):
+        return tf.nn.ctc_loss(
+            logits=y_pred["logits"],
+            logit_length=y_pred["logits_length"],
+            labels=y_true["labels"],
             label_length=y_true["labels_length"],
-            blank=self.blank,
+            logits_time_major=False,
+            blank_index=self.blank,
             name=self.name,
         )
-
-
-@tf.function
-def ctc_loss(
-    y_true,
-    y_pred,
-    input_length,
-    label_length,
-    blank,
-    name=None,
-):
-    return tf.nn.ctc_loss(
-        labels=tf.cast(y_true, tf.int32),
-        logit_length=tf.cast(input_length, tf.int32),
-        logits=tf.cast(y_pred, tf.float32),
-        label_length=tf.cast(label_length, tf.int32),
-        logits_time_major=False,
-        blank_index=blank,
-        name=name,
-    )

tensorflow_asr/losses/rnnt_loss.py
@@ -36,10 +36,10 @@ class RnntLoss(tf.keras.losses.Loss):
 
     def call(self, y_true, y_pred):
         return rnnt_loss(
-            logits=tf.cast(y_pred["logits"], tf.float32),
-            logit_length=tf.cast(y_pred["logits_length"], tf.int32),
-            labels=tf.cast(y_true["labels"], tf.int32),
-            label_length=tf.cast(y_true["labels_length"], tf.int32),
+            logits=y_pred["logits"],
+            logit_length=y_pred["logits_length"],
+            labels=y_true["labels"],
+            label_length=y_true["labels_length"],
             name=self.name,
         )
 
@@ -313,7 +313,6 @@ def compute_rnnt_loss_and_grad_helper(logits, labels, label_length, logit_length
     return loss, grads_logits
 
 
-@tf.function
 def rnnt_loss(
     logits,
     labels,