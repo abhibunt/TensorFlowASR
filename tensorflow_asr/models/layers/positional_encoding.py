c20cf377eb11fd6d6ceaf691dcc42d05bd55c8fc
Nguyễn Lê Huy <nlhuy.cs.16@gmail.com>
2022-10-03 13:06:44 +0700

    fix: update pe

--- tensorflow_asr/models/layers/positional_encoding.py
+++ tensorflow_asr/models/layers/positional_encoding.py
@@ -27,31 +27,32 @@ class PositionalEncoding(tf.keras.layers.Layer):
         dmodel = output_shape[-1]
         assert dmodel % 2 == 0, f"Input last dim must be even: {dmodel}"
 
-    def _create_encoding_matrix(self, sequence_length, max_length, dmodel):
-        pos = tf.expand_dims(tf.range(sequence_length - 1, -1, -1.0, dtype=tf.float32), axis=1)
+    def _create_encoding_matrix(self, batch_size, max_length, dmodel):
+        pos = tf.expand_dims(tf.range(max_length - 1, -1, -1.0, dtype=tf.float32), axis=1)
         index = tf.expand_dims(tf.range(0, dmodel, dtype=tf.float32), axis=0)
         pe_matrix = pos * (1 / tf.pow(self.scalar, (2 * (index // 2)) / dmodel))
-        # Sin cos will be [sequence_length, size // 2]
+        # Sin cos will be [max_length, size // 2]
         # we add 0 between numbers by using padding and reshape
         sin = tf.pad(tf.expand_dims(tf.sin(pe_matrix[:, 0::2]), -1), [[0, 0], [0, 0], [0, 1]], mode="CONSTANT", constant_values=0)
-        sin = tf.reshape(sin, [sequence_length, dmodel])
+        sin = tf.reshape(sin, [max_length, dmodel])
         cos = tf.pad(tf.expand_dims(tf.cos(pe_matrix[:, 1::2]), -1), [[0, 0], [0, 0], [1, 0]], mode="CONSTANT", constant_values=0)
-        cos = tf.reshape(cos, [sequence_length, dmodel])
-        # Then add sin and cos, which results in [sequence_length, dmodel]
+        cos = tf.reshape(cos, [max_length, dmodel])
+        # Then add sin and cos, which results in [max_length, dmodel]
         pe_matrix = tf.add(sin, cos)
-        # pad to [max_length, dmodel]
-        pe_matrix = tf.pad(pe_matrix, [[0, max_length - sequence_length], [0, 0]], mode="CONSTANT", constant_values=0)
+        pe_matrix = tf.repeat(pe_matrix, batch_size, axis=0)  # [B, max_length, dmodel]
         return pe_matrix
+
+    def _create_encoding_mask(self, inputs_length, max_length, dmodel):
+        pe_matrix_mask = tf.sequence_mask(inputs_length, maxlen=max_length, dtype=tf.float32)
+        pe_matrix_mask = tf.repeat(pe_matrix_mask, dmodel, axis=-1)
+        return pe_matrix_mask
 
     def call(self, inputs):
         outputs, inputs_length = inputs
-        _, max_length, dmodel = shape_util.shape_list(outputs)
-        list_inputs_length = tf.unstack(inputs_length, axis=0)
-        list_pe_matrix = tf.nest.map_structure(
-            lambda x: self._create_encoding_matrix(x, max_length=max_length, dmodel=dmodel),
-            list_inputs_length,
-        )
-        pe_matrix = tf.stack(list_pe_matrix, axis=0)
+        batch_size, max_length, dmodel = shape_util.shape_list(outputs)
+        pe_matrix = self._create_encoding_matrix(batch_size=batch_size, max_length=max_length, dmodel=dmodel)
+        pe_matrix_mask = self._create_encoding_mask(inputs_length=inputs_length, max_length=max_length, dmodel=dmodel)
+        pe_matrix = tf.multiply(pe_matrix, pe_matrix_mask)
         pe_matrix = tf.cast(pe_matrix, dtype=outputs.dtype)
         pe = tf.add(outputs, pe_matrix)
         return pe
