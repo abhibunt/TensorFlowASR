441d9413924cff7845a098b7b8f9f61d3dcce4ea
Nguyễn Lê Huy <nlhuy.cs.16@gmail.com>
2022-10-05 21:49:13 +0700

    fix: test mask

--- tensorflow_asr/models/encoders/conformer.py
+++ tensorflow_asr/models/encoders/conformer.py
@@ -454,7 +454,7 @@ class ConformerEncoder(tf.keras.Model):
         inputs_length = math_util.get_reduced_length(inputs_length, self.conv_subsampling.time_reduction_factor)
         outputs = self.linear(outputs, training=training)
         outputs = self.do(outputs, training=training)
-        attention_mask = compute_self_attention_mask(outputs, inputs_length)
+        # attention_mask = compute_self_attention_mask(outputs, inputs_length)
         if self._mha_type == "relmha":
             relative_position_encoding = compute_relative_position_encoding(shape_util.shape_list(outputs), dtype=outputs.dtype)
         else:
@@ -466,6 +466,5 @@ class ConformerEncoder(tf.keras.Model):
                 content_attention_bias=self.content_attention_bias,
                 positional_attention_bias=self.positional_attention_bias,
                 training=training,
-                attention_mask=attention_mask,
             )
         return outputs, inputs_length
