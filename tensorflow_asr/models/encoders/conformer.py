fix: update attention mask

tensorflow_asr/models/encoders/conformer.py
@@ -420,7 +420,7 @@ class ConformerEncoder(tf.keras.layers.Layer):
         inputs_length = math_util.get_reduced_length(inputs_length, self.conv_subsampling.time_reduction_factor)
         outputs = self.linear(outputs, training=training)
         outputs = self.do(outputs, training=training)
-        # attention_mask = compute_self_attention_mask(outputs, inputs_length)
+        attention_mask = compute_self_attention_mask(outputs, inputs_length)
         if self._mha_type == "relmha":
             relative_position_encoding = compute_relative_position_encoding(shape_util.shape_list(outputs), dtype=outputs.dtype)
         else:
@@ -430,6 +430,6 @@ class ConformerEncoder(tf.keras.layers.Layer):
                 outputs,
                 relative_position_encoding=relative_position_encoding,
                 training=training,
-                # attention_mask=attention_mask,
+                attention_mask=attention_mask,
             )
         return outputs, inputs_length

tensorflow_asr/models/layers/multihead_attention.py
@@ -29,11 +29,10 @@ def _rel_shift(x):
     return x
 
 
-def compute_self_attention_mask(inputs, inputs_length):  # [B] -> [B, T, T]
+def compute_self_attention_mask(inputs, inputs_length):  # [B] -> [B, Tquery, Tkey]
     _, max_length, _ = shape_util.shape_list(inputs)
-    mask = tf.expand_dims(tf.sequence_mask(inputs_length, maxlen=max_length, dtype=inputs.dtype), axis=-1)
-    mask = tf.matmul(mask, mask, transpose_b=True)
-    return mask
+    mask = tf.sequence_mask(inputs_length, maxlen=max_length, dtype=inputs.dtype)  # [B, Tquery]
+    return tf.tile(mask[:, :, None], [1, 1, max_length])  # [B, Tquery, Tkey]
 
 
 # def _rel_shift(x):