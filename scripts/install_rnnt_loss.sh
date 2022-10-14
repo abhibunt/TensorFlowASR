ee3c3f28ce74cce8ea19f37a39282743f8525782
Nguyễn Lê Huy <nlhuy.cs.16@gmail.com>
2022-10-15 00:56:25 +0700

    chore: update

--- scripts/install_rnnt_loss.sh
+++ scripts/install_rnnt_loss.sh
@@ -5,7 +5,7 @@ cd ./externals || exit
 
 # Install rnnt_loss
 if [ ! -d warp-transducer ]; then
-    git clone https://github.com/usimarit/warp-transducer.git
+    git clone https://github.com/HawkAaron/warp-transducer.git
 
     cd ./warp-transducer || exit
     mkdir build && cd build || exit
