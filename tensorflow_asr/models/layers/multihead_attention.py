fix: avoid using mask in softmax

tensorflow_asr/models/layers/multihead_attention.py
@@ -112,8 +112,6 @@ class MultiHeadAttention(tf.keras.layers.Layer):
         self.dropout = tf.keras.layers.Dropout(dropout, name="dropout")
         self._droput_rate = dropout
 
-        self.softmax = tf.keras.layers.Softmax(name="softmax")
-
     def build(self, input_shape):
         num_query_features = input_shape[0][-1]
         num_key_features = input_shape[1][-1]
@@ -192,14 +190,14 @@ class MultiHeadAttention(tf.keras.layers.Layer):
                 raise ValueError("mask's second to last dimension must be equal to the number of elements in 'query'")
             if key.shape[-3] != attention_mask.shape[-1]:
                 raise ValueError("mask's last dimension must be equal to the number of elements in 'key'")
+
+        attn_coef = tf.nn.softmax(logits)  # avoid using mask in softmax to avoid NaN gradients
+
         # apply mask
-        if attention_mask is not None:
-            # possibly expand on the head dimension so broadcasting works
+        if attention_mask is not None:  # possibly expand on the head dimension so broadcasting works
             if len(attention_mask.shape) != len(logits.shape):
                 attention_mask = tf.expand_dims(attention_mask, -3)
-
-        attn_coef = self.softmax(logits, mask=attention_mask)
-        attn_coef = tf.multiply(attn_coef, tf.cast(attention_mask, dtype=attn_coef.dtype))
+            attn_coef = tf.multiply(attn_coef, tf.cast(attention_mask, dtype=attn_coef.dtype))
 
         # attention dropout
         attn_coef_dropout = self.dropout(attn_coef, training=training)