7b8f2d1d4566ff26d7e5e0e2abe03c379652496d
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2022-10-16 19:52:31 +0700

    fix: update install rnnt loss

--- scripts/install_rnnt_loss.sh
+++ scripts/install_rnnt_loss.sh
@@ -3,12 +3,6 @@
 mkdir -p externals
 cd ./externals || exit
 
-if [ ! -d tensorflow ]; then
-  git clone https://github.com/tensorflow/tensorflow.git
-fi
-
-export TENSORFLOW_SRC_PATH=$(pwd)/tensorflow
-
 # Install rnnt_loss
 if [ ! -d warp-transducer ]; then
     git clone https://github.com/usimarit/warp-transducer.git
